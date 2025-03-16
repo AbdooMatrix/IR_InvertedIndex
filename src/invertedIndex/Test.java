2ff84d22f74a85447934068ba31047f3f0834d46
Abdelrahman Matrix <abdomostafa20188@gmail.com>
2025-03-16 21:27:39 +0200

    edit

--- src/invertedIndex/Test.java
+++ src/invertedIndex/Test.java
@@ -37,9 +37,6 @@ public class Test {
         index.store("index");
         index.printDictionary();
 
-        String test3 = "data  should plain greatest comif"; // data  should plain greatest comif
-        System.out.println("Boo0lean Model result = \n" + index.find_24_01(test3));
-
         String phrase = "";
 
         do {
